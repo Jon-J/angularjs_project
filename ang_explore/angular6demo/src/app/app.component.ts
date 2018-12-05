ca9af8270973e69753baa17710172f268407ff69
sheetal <sheetal.bv@dell.com>
2018-12-05 19:40:31 +0530

    test

--- ang_explore/angular6demo/src/app/app.component.ts
+++ ang_explore/angular6demo/src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './my_app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular6demo';
+  title = 'Sheeetal Demo';
 }
